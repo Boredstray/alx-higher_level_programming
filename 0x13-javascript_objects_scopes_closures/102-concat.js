54081ff511bfba3429ea69bdeebb9ddbf149e99a
Boredstray <eye.gabriel773@gmail.com>
2023-03-14 21:47:59 +0100

    ver1.1

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-const data1 = fs.readFileSync(process.argv[2], 'utf8');
-const data2 = fs.readFileSync(process.argv[3], 'utf8');
+const file1 = fs.readFileSync(process.argv[2], 'utf8');
+const file2 = fs.readFileSync(process.argv[3], 'utf8');
 
-fs.writeFileSync(process.argv[4], data1 + data2);
+fs.writeFileSync(process.argv[4], file1 + file2);

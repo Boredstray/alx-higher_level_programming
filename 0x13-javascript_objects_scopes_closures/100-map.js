23dcc38f9add407378179cb45717396c00dd7fcf
Boredstray <eye.gabriel773@gmail.com>
2023-03-14 21:24:03 +0100

    ver2.1

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,8 @@
 /* import a list */
 const lista = require('./100-data').list;
 
-const mappedList = list.map((current, index) => current * index);
-console.log(list);
-console.log(mappedList);
+const mapeado = lista.map((x, index) => {
+  return x * index;
+});
+console.log(lista);
+console.log(mapeado);
